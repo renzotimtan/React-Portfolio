db9afc8ea7694e3bc6b9db02520160963dc493d0
Renzo Tan <renzotimtan@gmail.com>
2021-01-29 19:26:00 +0800

    faq and services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,15 @@
-import AboutSection from '../components/AboutSection'
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
-    return ( 
-        <AboutSection />
-     );
-}
- 
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
+};
+
 export default AboutUs;
